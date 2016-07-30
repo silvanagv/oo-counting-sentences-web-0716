a99f2104ccd31d90e1579714a6ebc3eb3d7ebc98
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-07-30 14:46:25 -0400

    solved lab in long-winded way

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(". ")
+    binding.pry
+    array.length.times do |x|
+      if array[x].include?("!")
+        array[x] = array[x].split("!")
+      elsif array[x].include?("?")
+        array[x] = array[x].split("?")
+      end
+    end
+        array.flatten.count
   end
 end
